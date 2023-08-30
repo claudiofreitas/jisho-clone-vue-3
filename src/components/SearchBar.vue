a942efd697689b77aef5289aa6dab2319b87487b
Hans Hofner <hhofner@gmail.com>
2023-08-30 12:35:41 +0900

    feat: add icon magnifying glass icon component

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,15 +1,31 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import MagnifyGlass from './MagnifyGlass.vue'
+
+const emit = defineEmits<{
+  onSearch: [keyword: string]
+}>()
 
 const searchInput = ref<string | null>(null)
+const isOpened = ref(false)
+
+function submitSearch() {
+  if (searchInput.value) {
+    emit('onSearch', searchInput.value)
+  }
+}
 </script>
 
 <template>
-<div class="overlay">
-  <div class="inner">
-    <div class="type-selector">All</div>
-    <input type="text" name="keyword" tabindex="1" placeholder="English, Japanese, Romaji, words or text" v-model="searchInput"/>
-    <button>S</button>
+<div class="wrapper">
+  <div class="overlay" :class="{'opened-styles': isOpened}">
+    <div class="inner">
+      <div class="type-selector" @click="isOpened = !isOpened"><span>All</span><span>▾</span></div>
+      <input type="text" name="keyword" tabindex="1" placeholder="English, Japanese, Romaji, words or text" v-model="searchInput"/>
+      <button class="search-button" @click="submitSearch">
+        <MagnifyGlass />
+      </button>
+    </div>
   </div>
 </div>
 </template>
@@ -19,10 +35,20 @@ const searchInput = ref<string | null>(null)
   font-family: "Helvetica Neue";
 }
 
+.wrapper {
+  padding: 4px;
+}
+
 .type-selector {
   color: #eee;
   font-size: 14px;
   padding: 4px 12px;
+  color: #aaa;
+  display: flex;
+  gap: 3px;
+  border-right: solid 1px #888;
+  margin-right: 6px;
+  cursor: pointer;
 }
 
 input {
@@ -44,6 +70,7 @@ input {
   background: #6e6e6e;
   padding: 4px;
   border-radius: 5px;
+  transition: all 150ms ease-in-out;
 }
 
 .inner {
@@ -51,5 +78,17 @@ input {
   align-items: center;
   background: #404040;
   padding: 4px;
+  border-radius: 2px;
+}
+
+.opened-styles {
+  height: 400px;
+  padding: 8px;
+}
+
+.search-button {
+  background-color: #6a6a6a;
+  color: #ddd;
+  height: 36px;
 }
 </style>
